1fe6d5eba86f6ae41612f321055316b6372fc45e
usamah-apalah <musamahabdurrahman@gmail.com>
2026-07-15 10:08:42 +0700

    Delete E-learning-main/E-learning-main directory

--- E-learning-main/E-learning-main/app/utils/uploadthing.js
+++
@@ -1,8 +0,0 @@
-import {
-  generateUploadButton,
-  generateUploadDropzone,
-} from "@uploadthing/react";
-
-// pdfUploader adalah nama endpoint yang kita daftarkan di core.js sebelumnya
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
--- E-learning-main/E-learning-main/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Tambahkan baris di bawah ini
-  allowedDevOrigins: ["192.168.1.136"],
-};
-
-module.exports = nextConfig;
